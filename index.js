acf9098e2dc85d707b58d65504aa1ce69f5aab25
脆皮丶 <i@tsuiqg.me>
2024-05-09 16:19:26 +0800

    chore: release v1.1.0

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ module.exports = {
   printWidth: 120,
   semi: false,
   singleQuote: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
   trailingComma: "all",
   arrowParens: "avoid",
   endOfLine: "lf",
+  singleAttributePerLine: true,
+  vueIndentScriptAndStyle: false,
 };
