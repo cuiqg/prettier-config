da15f0806fae9224b685b34351145ba7e7a146a9
脆皮丶 <i@tsuiqg.me>
2024-05-10 13:28:40 +0800

    chore: release v1.1.1

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  printWidth: 120,
   semi: false,
   singleQuote: true,
   bracketSpacing: true,
   bracketSameLine: true,
-  trailingComma: "all",
+  trailingComma: "es5",
   arrowParens: "avoid",
   endOfLine: "lf",
+  tabWidth: 2,
   singleAttributePerLine: true,
   vueIndentScriptAndStyle: false,
+  plugins: [
+    "prettier-plugin-packagejson",
+    "prettier-plugin-tailwindcss",
+    "@shufo/prettier-plugin-blade",
+    "@trivago/prettier-plugin-sort-imports",
+  ],
+  overrides: [
+    {
+      files: "*.blade.php",
+      options: {
+        parser: "blade",
+      },
+    },
+  ],
 };
