af2104ccad4b8989bbe450958d8ea59ce2823b31
脆皮丶 <i@tsuiqg.me>
2024-07-06 22:13:11 +0800

    chore: release v1.1.6

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-/** @type {import('prettier').Config} */
-module.exports = {
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
   semi: false,
   singleQuote: true,
   bracketSpacing: true,
   bracketSameLine: true,
-  trailingComma: 'es5',
+  trailingComma: 'none',
   arrowParens: 'avoid',
   endOfLine: 'lf',
+  useTabs: false,
   tabWidth: 2,
   singleAttributePerLine: true,
   vueIndentScriptAndStyle: false,
+  editorconfig: true,
   plugins: [],
   overrides: [],
 }
+
+export default config
